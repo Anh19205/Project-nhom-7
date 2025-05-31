d7a5f793518418dd706ff74ed6709e8ea1d817cd
NguyenHuynhNhatNam <namnhn2921@ut.edu.vn>
2025-05-31 17:31:39 +0700

    time format created

---
+++ Hannah-HoTroAIChoGiaoDucCongNghePhanMem/post-service/src/main/java/com/hannah/post_service/dto/response/PostResponse.java
@@ -0,0 +1,20 @@
+package com.hannah.post_service.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PostResponse {
+    String id;
+    String content;
+    String userId;
+    String createdTime;
+    Instant createdDate;
+    Instant modifiedDate;
+}
