c909dc1529db41d72d93ed44664f6ba397708a34
Toann06 <minhtoan2005z@gmail.com>
2025-06-01 14:52:51 +0700

    get name from profile service

---
+++ Hannah-HoTroAIChoGiaoDucCongNghePhanMem/identity-service/src/main/java/com/hannah/identity_service/dto/request/PermissionRequest.java
@@ -0,0 +1,17 @@
+package com.hannah.identity_service.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionRequest {
+    private String name;
+    private String description;
+}
